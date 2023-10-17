81d0ee9042e8b84b47f12bbed01041f9bd32c596
JoshZ520 <joshuazob@gmail.com>
2023-10-17 16:25:24 -0600

    dark mode

---
+++ scripts/darkmode.js
@@ -0,0 +1,20 @@
+const darkmode = document.querySelector("#dark-mode");
+const bodyelt = document.querySelector("body");
+
+darkmode.addEventListener("click", () => {
+  if (darkmode.textContent == "Dark Mode") {
+    document.documentElement.style.setProperty("--background-color", "black");
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--head-background", "#1b264f");
+    document.documentElement.style.setProperty("--links", "#f5f3f5");
+    bodyelt.style.backgroundColor = "black";
+    darkmode.textContent = "Light Mode";
+  } else {
+    document.documentElement.style.setProperty("--background-color", "#74c7bd");
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--head-background", "#274690");
+    document.documentElement.style.setProperty("--links", "#302b27");
+    bodyelt.style.backgroundColor = "#74c7bd";
+    darkmode.textContent = "Dark Mode";
+  }
+});
