585af0c244526c732a37bbf965bdda2ea53a80e3
JoshZ520 <joshuazob@gmail.com>
2023-11-11 19:58:44 -0700

    updates to dark

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -26,3 +26,32 @@ darkmode.addEventListener("click", () => {
     darkmode.textContent = "Dark Mode";
   }
 });
+
+const darkmodeC = document.querySelector("#dark-mode-c");
+const bodyeltC = document.querySelector("body");
+
+darkmodeC.addEventListener("click", () => {
+  if (darkmode.textContent == "Dark Mode") {
+    document.documentElement.style.setProperty("--background-color", "black");
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--head-background", "#1b264f");
+    document.documentElement.style.setProperty("--links", "#f5f3f5");
+    document.documentElement.style.setProperty("--hover-color", "#2A3C7A");
+    document.documentElement.style.setProperty(
+      "--second-background",
+      "#373f51"
+    );
+
+    bodyelt.style.backgroundColor = "black";
+    darkmode.textContent = "Light Mode";
+  } else {
+    document.documentElement.style.setProperty("--background-color", "#74c7bd");
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--head-background", "#3057b0");
+    document.documentElement.style.setProperty("--links", "black");
+    document.documentElement.style.setProperty("--hover-color", "royalblue");
+    document.documentElement.style.setProperty("--second-background", "white");
+    bodyelt.style.backgroundColor = "#74c7bd";
+    darkmode.textContent = "Dark Mode";
+  }
+});
