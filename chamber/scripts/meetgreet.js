e79c8e370478dd628a98ccb8d13b09e76b9fe33b
JoshZ520 <joshuazob@gmail.com>
2023-11-16 15:57:35 -0700

    updated banner

--- chamber/scripts/meetgreet.js
+++ chamber/scripts/meetgreet.js
@@ -1,7 +1,7 @@
 let meetdate = new Date();
 let dayofweek = meetdate.getDay();
 
-if (dayofweek == 1 || dayofweek == 2 || dayofweek == 3) {
+if (dayofweek == 1 || dayofweek == 2 || dayofweek == 4) {
   let meetgreet = document.getElementById("meetgreet");
   meetgreet.style.display = "block";
 }
