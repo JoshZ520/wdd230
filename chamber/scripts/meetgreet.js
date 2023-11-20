e158ed7e1c460ec555afc6074244969081c308f9
JoshZ520 <joshuazob@gmail.com>
2023-11-20 14:18:54 -0700

    added closing the meet greet banner

--- chamber/scripts/meetgreet.js
+++ chamber/scripts/meetgreet.js
@@ -1,7 +1,14 @@
 let meetdate = new Date();
 let dayofweek = meetdate.getDay();
 
-if (dayofweek == 1 || dayofweek == 2 || dayofweek == 4) {
+if (dayofweek == 1 || dayofweek == 2 || dayofweek == 3) {
   let meetgreet = document.getElementById("meetgreet");
   meetgreet.style.display = "block";
 }
+
+let x = document.getElementById("close-greet");
+
+x.addEventListener("click", () => {
+  let meetgreet = document.getElementById("meetgreet");
+  meetgreet.style.display = "none";
+});
