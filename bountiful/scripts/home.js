c36fb810fb361fe77379f27ce10f5b088a2cce73
JoshZ520 <joshuazob@gmail.com>
2023-12-09 13:35:56 -0700

    updates

--- bountiful/scripts/home.js
+++ bountiful/scripts/home.js
@@ -7,9 +7,11 @@ const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=$
 //const apiURL = "data/weather.json";
 
 function displayWeather(weatherData) {
+  // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
   // const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
   const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
+  const windSpeed = weatherData.wind.speed.toFixed(0);
   const temperature = weatherData.main.temp.toFixed(0);
 
   //Set up the weather icon
@@ -22,10 +24,7 @@ function displayWeather(weatherData) {
   weatherDesc.innerHTML = `${desc}`;
 
   let weatherTemp = document.getElementById("temp");
-  weatherTemp.innerHTML = `${temperature}&deg;F`;
-
-  let humidity = document.getElementById("humidity");
-  weatherTemp.innerHTML = `${humidity}%`;
+  weatherTemp.innerHTML = `${temperature}&deg;F | ${windSpeed} mph wind`;
 }
 
 async function getTheWeather() {
@@ -44,40 +43,55 @@ async function getTheWeather() {
 
 getTheWeather();
 
-// const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
-// const ONE_DAY = 24 * 60 * 60 * 1000;
+const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
+const ONE_DAY = 24 * 60 * 60 * 1000;
+function showForecast(forecasts) {
+  console.log(forecasts);
+  let dates = [];
+  let mydate = new Date();
+  for (let i = 0; i < 3; i++) {
+    mydate = new Date(mydate.getTime() + ONE_DAY);
+    nextdate = mydate.toISOString().slice(0, 10);
+    dates.push(nextdate);
+  }
+  console.log(dates);
 
-// function showForecast(forecasts) {
-//   console.log(forecasts);
-//   let dates = [];
-//   let mydate = new Date();
-//   for (let i = 0; i < 3; i++) {
-//     mydate = new Date(mydate.getTime() + ONE_DAY);
-//     nextdate = mydate.toISOString().slice(0, 10);
-//     dates.push(nextdate);
-//   }
-//   console.log(dates);
+  highTemps = dates.map((date) =>
+    forecasts
+      .filter((x) => x.dt_txt.startsWith(date))
+      .reduce((prev, next) => (prev.main.temp < next.main.temp ? prev : next))
+  );
 
-//   weatherElt = document.getElementById("three-day");
-//   for (let i = 0; i < 3; i++) {
-//     let newsection = document.createElement("section");
-//     newsection.innerHTML = `<h2>${dates[i]}</h2> <img id="weather-icon" src="" alt=""/>`;
-//     weatherElt.append(newsection);
-//   }
-// }
+  lowTemps = dates.map((date) =>
+    forecasts
+      .filter((x) => x.dt_txt.startsWith(date))
+      .reduce((prev, next) => (prev.main.temp < next.main.temp ? prev : next))
+  );
 
-// async function fetchForecast() {
-//   try {
-//     const response = await fetch(weatherURL);
-//     if (response.ok) {
-//       const data = await response.json();
-//       showForecast(data.list);
-//     } else {
-//       throw Error(await response.text());
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+  weatherElt = document.querySelector("body span");
+  for (let i = 0; i < 3; i++) {
+    let newsection = document.createElement("section");
+    newsection.innerHTML = `<h2>${dates[i]}</h2><p>High: ${highTemps[
+      i
+    ].main.temp.toFixed(0)}&deg;</p><p>Low: ${lowTemps[i].main.temp.toFixed(
+      0
+    )}&deg;</p>`;
+    weatherElt.append(newsection);
+  }
+}
 
-// fetchForecast();
+async function fetchForecast() {
+  try {
+    const response = await fetch(weatherURL);
+    if (response.ok) {
+      const data = await response.json();
+      showForecast(data.list);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchForecast();
